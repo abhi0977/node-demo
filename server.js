4364b23d97b8a5e1021f83e60bd5e1f57e63051d
Abhi Ramani <abhiramani3@gmail.com>
2018-08-24 13:05:54 +0530

    Basic connection working

--- server.js
+++ server.js
@@ -1,78 +1,34 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-// Using express: http://expressjs.com/
-var express = require('express');
-// Create the app
-var app = express();
-var app1 = express();
-var sockettemp;
-// Set up the server
-// process.env.PORT is related to deploying on heroku
-var server = app.listen(3000);
-var server1 = app1.listen(4000);
+server.listen(3000);
+// WARNING: app.listen(80) will NOT work here!
 
-app.use('/', express.static('public'))
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
-app.use('/static', express.static('p2'))
+app.get('/play', function (req, res) {
+  res.sendFile(__dirname + '/play/index.html');
+});
 
-var io = require('socket.io');
+io.on('connection', function (socket) {
+  console.log("Connected",socket.id);
 
-localhost:3000/2736/gameon
-localhost:3000/2736/gameon/play
+  socket.on('join', function (data) {
+    console.log("user joined with email : ", data.email);
+    socket.join(data.email);
+  });
 
-// WebSocket Portion
-// WebSockets work with the HTTP server
-var io1 = require('socket.io')(server1);
-
-// Register a callback function to run when we have an individual connection
-// This is run for each individual user that connects
-io.on('connection',
-  // We are given a websocket object in our function
-  function (socket) {
-sockettemp = socket
-    console.log("We have a new client: " + socket.id);
-  
-    // When this user emits, client side: socket.emit('otherevent',some data);
-    socket.on('/2736/gameon',
-      function(data) {
-        // // Data comes in as whatever was sent, including objects
-        // console.log("Received: 'mouse' " + data.x);
-      
-        // // Send it to all other clients
-        // socket.broadcast.emit('mouse', data);
-        
-        // // This is a way to send to everyone including sender
-        // // io.sockets.emit('message', "this goes to everyone");
-
-      }
-    );
-  }
-);
+  socket.on('send', function (data) {
+    console.log("user " + data.email + " sent the ans : " + data.ans);
+    io.sockets.in(data.email).emit('ans', {ans: data.ans === 'C'});
+  });
 
 
-io1.sockets.on('connection',
-  // We are given a websocket object in our function
-  function (socket) {
-
-
-    console.log("We have a vary new client: " + socket.id);
-  
-    // When this user emits, client side: socket.emit('otherevent',some data);
-    socket.on('xyz',
-      function(data) {
-        // Data comes in as whatever was sent, including objects
-        console.log("Received: 'mouse' " + data.x);
-      
-        // Send it to all other clients
-        // socket.broadcast.emit('mouse', data);
-        
-        // This is a way to send to everyone including sender
-        // io.sockets.emit('message', "this goes to everyone");
-
-      }
-    );
-  }
-);
-
-functionsdkfj()
-sockettemp.broadcast
-{}
+  // socket.emit('news', { hello: 'world' });
+  // socket.on('my other event', function (data) {
+  //   console.log(data);
+  // });
+});
